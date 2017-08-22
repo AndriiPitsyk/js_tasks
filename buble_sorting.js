56fd491d4a5f499d922feef84f8eaed9d20f8456
AndriiPitsyk <andrii.pitsyk@techmagic.co>
2017-08-22 21:49:18 +0300

    buble sorting

---
+++ buble_sorting.js
@@ -0,0 +1,14 @@
+var a = [3,5,2,1,4,9,12,7];
+var length = a.length;
+
+for (var i = length ; i >= 0; i--) {
+	for (var j = 0; j < length; j++) {
+		if (a[j] > a[j + 1]) {
+			var t = a[j];
+			a[j] = a[j + 1];
+			a[j + 1] = t;
+      	}
+    }
+}
+
+console.log(a[i])
